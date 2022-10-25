fbbf624ef56d13aa4ee54011156f5391bda6a3f8
D <101846054+ositococacola@users.noreply.github.com>
2022-10-24 23:42:16 -0300

    api key agregada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'd22bf6e408f4d6171c4a4941209ab0ef',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
