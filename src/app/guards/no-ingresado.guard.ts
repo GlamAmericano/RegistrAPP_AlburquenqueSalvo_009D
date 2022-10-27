55eac115ba48bedabeaf993349a3e7fec033c4b4
D <101846054+ositococacola@users.noreply.github.com>
2022-10-27 10:11:03 -0300

    Guards agregados

---
+++ src/app/guards/no-ingresado.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoIngresadoGuard implements CanActivate {
+
+  constructor (private navCtrl: NavController){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if(localStorage.getItem('ingresado')=='true'){
+        this.navCtrl.navigateRoot('home');
+        return false;
+      }
+      else
+        {
+        return true;
+        }
+    }
+  }
+
+  
